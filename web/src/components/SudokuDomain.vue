2cbc1b5eafd5a6a4486faeff28f0598a6f43f222
abuco <52994294+abu-co@users.noreply.github.com>
2025-11-20 21:35:19 +1100

    Remove example code

---
+++ web/src/components/SudokuDomain.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  domain: Set<number>;
+}>();
+</script>
+
+<template>
+  <div class="grid grid-cols-3 grid-rows-3">
+    <div v-for="value in 9" :key="value" :class="{
+      'bg-neutral-500/60 transition-opacity': true,
+      'opacity-40': !domain.has(value)
+    }" />
+  </div>
+</template>
